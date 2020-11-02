b10d448b7427acdfbce35cdb3f5a9ffcc9528f90
gillian.deville <gillian.deville@epita.fr>
2020-11-02 22:01:36 +0100

    [GET][FIX]: moved get to router

--- app/routes/index.js
+++ app/routes/index.js
@@ -2,6 +2,11 @@ const express = require("express");
 
 const router = express.Router();
 
+router.use(function timelog(req, res, next) {
+  console.log("Time: ", Date.now());
+  next();
+});
+
 /* GET home page. */
 router.get("/", (req, res) => {
   console.log("coucou");
