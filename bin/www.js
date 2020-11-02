b2c7d0f391ed37d8157b912b4b113c7cfa6b8a01
gillian.deville <gillian.deville@epita.fr>
2020-11-02 22:04:39 +0100

    [WWW][FIX]: moved back to http server

--- bin/www.js
+++ bin/www.js
@@ -1,5 +1,5 @@
 const winston = require("winston");
-// const http = require("http");
+const http = require("http");
 const config = require("../config/config");
 
 const Log = winston.createLogger({
@@ -41,7 +41,7 @@ app.set("port", port);
 //   res.write("Hello World!");
 //   res.end();
 // });
-// const server = http.createServer(app);
+const server = http.createServer(app);
 
 function onError(error) {
   if (error.syscall !== "listen") {
@@ -64,11 +64,12 @@ function onError(error) {
 }
 
 function onListening() {
-  // Log.info(`Listening on ${}:${addr.port}`)
-  Log.info("Listening Server");
+  const addr = server.address()
+  Log.info(`Listening on ${addr.addr}:${addr.port}`)
+  // Log.info("Listening Server");
 }
 
-app.listen(port, onListening);
+server.listen(port, config.main.listenOn);
 
-// server.on("error", onError);
-// server.on("listening", onListening);
+server.on("error", onError);
+server.on("listening", onListening);
